757e075d1ac8c251c68140b59c3b2c21d96db731
junpeiwada <junpeiwada@gmail.com>
2016-08-30 17:12:09 +0900

    動画のサムネイルを表示するようにした

--- MoviePlayer/VideoListViewController.swift
+++ MoviePlayer/VideoListViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import AVKit
+import AVFoundation
 
 
 class VideoListViewController: UITableViewController {
@@ -48,11 +49,42 @@ class VideoListViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("test")
-        let l = (cell?.viewWithTag(100) as? UILabel)!
+        let l = (cell?.viewWithTag(1001) as? UILabel)!
         l.text = files[indexPath.row]
         
+        let image1 = (cell?.viewWithTag(10) as? UIImageView)!
+        image1.image = self.createThumbnail(filePaths[indexPath.row],location: 0.1)
+        
+        let image2 = (cell?.viewWithTag(11) as? UIImageView)!
+        image2.image = self.createThumbnail(filePaths[indexPath.row],location: 0.3)
+        
+        let image3 = (cell?.viewWithTag(12) as? UIImageView)!
+        image3.image = self.createThumbnail(filePaths[indexPath.row],location: 0.6)
+        
+        let image4 = (cell?.viewWithTag(13) as? UIImageView)!
+        image4.image = self.createThumbnail(filePaths[indexPath.row],location: 0.85)
         return cell!
     }
+    
+    func createThumbnail(path:String,location:Double) -> UIImage{
+        let url = NSURL(fileURLWithPath:path)
+        let asset = AVURLAsset(URL: url)
+        asset.tracksWithMediaCharacteristic(AVMediaTypeVideo)
+        
+        let imageGen = AVAssetImageGenerator(asset: asset)
+        let durationSeconds = CMTimeGetSeconds(asset.duration)
+       
+        let midpoint = CMTimeMakeWithSeconds(durationSeconds*location, 600)
+        do{
+            let hafwatImage = try imageGen.copyCGImageAtTime(midpoint, actualTime: nil)
+            let image = UIImage(CGImage: hafwatImage)
+            return image
+        }catch{
+            print("Error")
+            return UIImage()
+        }
+    }
+    
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let player = self.storyboard?.instantiateViewControllerWithIdentifier("Vplayer") as! VideoPlayerVC
